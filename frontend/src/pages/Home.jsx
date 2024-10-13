32615309885453c5a3b5d508b39a9e2567ea984d
shamsrehmanpro <shamsrehmanpro@gmail.com>
2024-10-13 18:00:30 +0500

    adding best seller components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection.jsx'
+import BestSeller from '../components/BestSeller.jsx'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
     </div>
   )
 }
